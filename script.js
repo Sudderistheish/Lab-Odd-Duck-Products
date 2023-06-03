d8ed29943f7972150994d83d619a887409353abc
Andrea Malone <andreamurry63@gmail.com>
2023-06-03 15:57:25 -0500

    dog duck Changes

---
+++ script.js
@@ -0,0 +1,19 @@
+let votingArea = document.getElementById('voting'-area);
+let resultArea = document.getElementById('result-area');
+
+let box = document.getElementById("clicker");
+
+function handleBoxClick(event) {
+event.preventDefault();
+let target = event.target;
+target.style.backgroundColor = "yellow";
+target.dataset.n = "1001"
+}
+
+ box.addEventListener("click"), handleBoxClick);
+
+let box1 = document.getElementById("clicker-d0g-duck");
+
+let box2 = document.getElementById("clicker-unicorn");
+
+let box3 = document.getElementById("clicker-bugglegum");
