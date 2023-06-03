9ec6f0c4fd250146d506b5fd9328cb5e74ec2aac
Andrea Malone <andreamurry63@gmail.com>
2023-06-03 18:11:14 -0500

    more updates to java script

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 'use strict';
 
-let votingArea = document.getElementById('voting'-area);
+let votingArea = document.getElementById('voting-area');
 let resultArea = document.getElementById('result-area');
 
-let box = document.getElementById("clicker");
+let dog1Img = document.getElementById('duck-dog');
+let dog2Img = document.getElementById('unicorn');
+let dog3Img = document.getElementById ('bubblegum');
+
+//dog contrustor it has a name image vote count and view count
+function Dogpproduct(name, imgSrc);{
+    this.name = name2;
+    this.imgSrc = imgSrc;
+    this.voteCount = 0;
+    this.viewcount = 0;
+    
+}
+ let dogDuck = new Dog("dogDuck", "./img/dog-duck.jpg");
+ let floatdogDuck = new Dog ("FloatYourDogduck", 
+ 
+ dogArray.push(dogDuck);
+ dogArray.push(floatdogDuck);
+
+let dogArray = [];
+
+console.log(dogArray);
+
+
+
+
 
 function handleBoxClick(event) {
 event.preventDefault();
 let target = event.target;
 target.style.backgroundColor = "yellow";
 target.dataset.n = "1001"
+
+
 }
 
  box.addEventListener("click"), handleBoxClick);
