f306d0cfad0c346cabc995f2bbd1588dfec5808a
Andrea Malone <andreamurry63@gmail.com>
2023-06-15 21:39:25 -0500

    trying to fix code

--- script.js
+++ script.js
@@ -1,12 +1,67 @@
+'use strict';
 console.log("Hello world")
-function Products(name,Src) {
+function Product(name,Src) {
     this.name = name;
     this.Src = Src;
     this.clicks = 0;
     this.views = 0;
+    this.imgSrc = imgSrc;
+    this.voteCount = 0;
+    this.viewcount = 0;
 }
 
-let dogDuck = new Products("dogDuck","./img/dog-duck.jpg")
-console.log(dogDuck);
 
-dogDuckArray.push(dogDuck);
+
+let product1 = new product1("dog-Duck","./img/dog-duck.jpg");
+let product2 = new product2("unicorn","./img/unicorn.jpg");
+let product3 = new product3("bubblegum","./img/bubblegum.jpg");
+
+
+ 
+
+ productArray.push(dogDuck);
+ productArray.push(unicorn);
+ productArray.push(bubblegum);
+ 
+
+let productArray = [];
+
+console.log(productArray);
+
+function setProductImages(product1, product2, product3){
+
+
+
+product1Img.src = product1.imgSrc;
+product1Img.alt = product1.name;
+product1Img.title =poduct1.name;
+product2Img.src = product2Img.imgSrc;
+product2Img.alt = product2.name;
+product3Img.title = product2.name;
+product3Img.src = product3.imgSrc;
+product3Img.alt = product3.name;
+product3.title = product3.name;
+
+}
+
+setProductImages(dogArray [0], dogArray [0]);
+
+renderProductImages(dogDuck, unicorn, bubblegum,);
+
+function handleBoxdogClick(event) {
+event.preventDefault();
+let target = event.target;
+let dogName = target.alt;
+
+}
+
+let theBestProduct;
+for (let i=0; i<productArray.length; i++) {
+    let product = productArray [i];
+    if(dog.name === dogName) {
+    theBestDog = DogPoduct; 
+        
+    }
+
+}
+
